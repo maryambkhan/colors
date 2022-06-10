d22dca71a5f50c6860add9dde1c157c6ad7c192c
Maryam Khan <97411734+maryambkhan@users.noreply.github.com>
2022-06-10 15:25:17 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const colors = ["green","red","rgba(133,122,200)","#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListner("click". function() {
+
+const randomNumber = 2;
+document.body.style.backgroundColor = colors[randomNumbers];
+color.textContent = colors[randomNumber];
+});
