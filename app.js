b9ff69e7c609579c61669798be62088d0a547e09
Maryam Khan <97411734+maryambkhan@users.noreply.github.com>
2022-06-10 15:36:05 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const btn = document.getElementById('btn');
 const color = document.querySelector(".color");
 
 btn.addEventListner("click". function() {
-
 const randomNumber = 2;
+                    
 document.body.style.backgroundColor = colors[randomNumbers];
 color.textContent = colors[randomNumber];
 });
+
+function getRandomNumber(){
+  return Math.floor(Math.random() * colors.length);
+}
