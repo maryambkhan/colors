d7873825c693b9b9a84a915ef9d36a81e032e43c
Maryam Khan <97411734+maryambkhan@users.noreply.github.com>
2022-06-10 16:33:37 -0600

    Update hex.js

--- hex.js
+++ hex.js
@@ -3,5 +3,10 @@ const btn = document.getElementById("btn");
 const color = document.querySelecter(".color");
 
 btn.addEventListener("click", function(){
-  
-})
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++){
+    hexColor += hex[0];
+  }
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
